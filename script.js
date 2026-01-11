12aa658e0561dc5a9a2a726931028c23ebf87160
amanbaevv <amanbaevvv09@gmail.com>
2026-01-11 21:55:26 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// const f = ()=>{
-//     let text = document.getElementById('area').value
-//     console.log(text)
-//     let gg = document.getElementById("g").innerHTML=text
-// }
-let elem = document.getElementById("names")
-
-const tets = ()=>{
- elem.innerHTML = "salam"
-document.body.style.backgroundColor='red'
-document.body.style.backgroundImage='url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ6z3AvO1SENsBK5vTg38y1erx6lrEjFlPZA&s)'
+const f = ()=>{
+    let text = document.getElementById('area').value
+    console.log(text)
+    let gg = document.getElementById("g").innerHTML=text
 }
+// let elem = document.getElementById("names")
+
+// const tets = ()=>{
+//  elem.innerHTML = "salam"
+// document.body.style.backgroundColor='red'
+// document.body.style.backgroundImage='url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ6z3AvO1SENsBK5vTg38y1erx6lrEjFlPZA&s)'
+// }
 
 
 
